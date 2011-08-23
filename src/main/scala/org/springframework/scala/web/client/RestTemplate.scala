34599643d549928ca8c58c99cc3feda7001387bc
Arjen Poutsma <apoutsma@vmware.com>
2011-08-23 14:24:27 +0200

    Using scalaj_collection

--- src/main/scala/org/springframework/scala/web/client/RestTemplate.scala
+++ src/main/scala/org/springframework/scala/web/client/RestTemplate.scala
@@ -1,33 +1,32 @@
 package org.springframework.scala.web.client
 
-import org.springframework.http.client.ClientHttpRequestFactory
 import java.net.URI
+import scalaj.collection.Imports._
 
-class RestTemplate private (val javaTemplate: org.springframework.web.client.RestTemplate) {
+class RestTemplate (val javaTemplate: org.springframework.web.client.RestOperations) {
 
+  def this() {
+    this(new org.springframework.web.client.RestTemplate())
+  }
+  
   // DELETE
-  def delete(url: String, urlVariables: AnyRef*) {
-    javaTemplate.delete(url, urlVariables: _*)
+
+  def delete(url: String, urlVariables: Any*) {
+    javaTemplate.delete(url, asInstanceOfAnyRef(urlVariables): _*)
   }
 
-  def delete(url: String, urlVariables: Map[String, _]) {
-    javaTemplate.delete(url, urlVariables)
+  def delete[T](url: String, urlVariables: Map[String, T]) {
+    val javaMap = urlVariables.asJava[String, T]
+    javaTemplate.delete(url, javaMap)
   }
 
   def delete(url: URI) {
     javaTemplate.delete(url)
   }
 
-}
+  private def asInstanceOfAnyRef(seq: Seq[Any]) = {
+    seq.map(_.asInstanceOf[AnyRef]);
+  }
 
-object RestTemplate {
-
-  def apply() = new RestTemplate(new org.springframework.web.client.RestTemplate())
-
-  def apply(requestFactory: ClientHttpRequestFactory) =
-    new org.springframework.web.client.RestTemplate(requestFactory)
-
-  def apply(javaTemplate: org.springframework.web.client.RestTemplate) =
-    new RestTemplate(javaTemplate)
 
 }
---
+++ src/test/scala/org/springframework/scala/web/client/RestTemplateTest.scala
@@ -0,0 +1,43 @@
+package org.springframework.scala.web.client
+
+;
+
+import org.scalatest.mock.EasyMockSugar
+import org.springframework.web.client.RestOperations
+import java.util.HashMap
+import org.easymock.EasyMock
+import org.scalatest.{BeforeAndAfter, FunSuite}
+
+class RestTemplateTest extends FunSuite with EasyMockSugar with BeforeAndAfter {
+
+  private val mockJavaTemplate = strictMock[RestOperations]
+
+  private val scalaTemplate = new RestTemplate(mockJavaTemplate)
+
+  before {
+    EasyMock.reset(mockJavaTemplate)
+  }
+
+  test("delete with varargs") {
+    expecting {
+      mockJavaTemplate.delete("http://{e}xample.com", 'e'.asInstanceOf[AnyRef])
+    }
+
+    whenExecuting(mockJavaTemplate) {
+      scalaTemplate.delete("http://{e}xample.com", 'e')
+    }
+  }
+
+  test("delete with map") {
+    expecting {
+      val variables: java.util.Map[String, String] = new HashMap[String, String]
+      variables.put("host", "example")
+      mockJavaTemplate.delete("http://{host}.com", variables)
+    }
+
+    whenExecuting(mockJavaTemplate) {
+      scalaTemplate.delete("http://{host}.com", Map("host" -> "example"))
+    }
+  }
+
+}
