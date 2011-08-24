433e0724faf361470b439670affcc2921d2ec9aa
Arjen Poutsma <apoutsma@vmware.com>
2011-08-24 15:21:31 +0200

    Using scalatest

--- src/test/scala/org/springframework/scala/web/client/ScalaDriver.scala
+++ src/test/scala/org/springframework/scala/web/client/ScalaDriver.scala
@@ -1,10 +1,21 @@
 package org.springframework.scala.web.client
 
+import java.net.URI
+import org.springframework.http.HttpMethod
+import org.springframework.http.client.ClientHttpResponse
+import org.springframework.util.FileCopyUtils
+import java.io.InputStreamReader
+
 object ScalaDriver {
 
   def main(args: Array[String]) {
     val template = new RestTemplate()
-    template.delete("http://localhost")
+//    template.delete("http://localhost")
+
+    val result = template.execute(new URI("http://localhost"), HttpMethod.GET) {
+      response: ClientHttpResponse => FileCopyUtils.copyToString(new InputStreamReader(response.getBody))
+    }
+    println(result.getClass)
 
 
   }
