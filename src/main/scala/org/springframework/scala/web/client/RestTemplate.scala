43b75c7da41bd2470fe4e383d04b2fea31fb5fd1
Arjen Poutsma <apoutsma@vmware.com>
2011-08-26 14:37:47 +0200

    Started on JdbcTemplate

--- src/main/scala/org/springframework/scala/web/client/RestTemplate.scala
+++ src/main/scala/org/springframework/scala/web/client/RestTemplate.scala
@@ -1,10 +1,26 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.scala.web.client
 
 import java.net.URI
 import scalaj.collection.Imports._
 import org.springframework.web.client.{RequestCallback, ResponseExtractor}
 import org.springframework.http.HttpMethod
-import org.springframework.http.client.{ClientHttpRequest, ClientHttpResponse}
+import org.springframework.http.client.ClientHttpResponse
 
 class RestTemplate(val javaTemplate: org.springframework.web.client.RestOperations) {
 
@@ -25,14 +41,9 @@ class RestTemplate(val javaTemplate: org.springframework.web.client.RestOperatio
     javaTemplate.delete(url)
   }
 
-  def execute[T](url: URI, method: HttpMethod)
-                (requestClosure: ClientHttpRequest => Unit)
+  def execute[T](url: URI, method: HttpMethod, requestCallback: RequestCallback)
                 (responseClosure: ClientHttpResponse => T) = {
-    javaTemplate.execute(url, method, new RequestCallback {
-      def doWithRequest(request: ClientHttpRequest) {
-
-      }
-    }, new ResponseExtractor[T] {
+    javaTemplate.execute(url, method, requestCallback, new ResponseExtractor[T] {
       def extractData(response: ClientHttpResponse) = {
         responseClosure(response)
       }
