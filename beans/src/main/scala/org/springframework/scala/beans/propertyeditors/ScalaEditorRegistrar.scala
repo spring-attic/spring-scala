24e4fe429f8fa3d1c8d0a3f78bea184b616ed2b8
Arjen Poutsma <apoutsma@vmware.com>
2012-02-28 11:09:09 +0100

    Added SeqEditor

---
+++ beans/src/main/scala/org/springframework/scala/beans/propertyeditors/ScalaEditorRegistrar.scala
@@ -0,0 +1,31 @@
+package org.springframework.scala.beans.propertyeditors
+
+/*
+ * Copyright 2011-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.springframework.beans.{PropertyEditorRegistry, PropertyEditorRegistrar}
+import scala.collection.mutable.ArrayBuffer
+
+/**
+ * @author Arjen Poutsma
+ */
+class ScalaEditorRegistrar extends PropertyEditorRegistrar {
+
+  def registerCustomEditors(registry: PropertyEditorRegistry) {
+    registry.registerCustomEditor(classOf[ArrayBuffer[Any]], new SeqEditor(classOf[ArrayBuffer[Any]]))
+    registry.registerCustomEditor(classOf[Seq[Any]], new SeqEditor(classOf[Seq[Any]]))
+  }
+}
