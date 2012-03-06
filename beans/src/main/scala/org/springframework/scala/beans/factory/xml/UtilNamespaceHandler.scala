78c5957421cfacdf1908a0d2a362194fac093acf
Arjen Poutsma <apoutsma@vmware.com>
2012-03-06 15:33:56 +0100

    Working on scala-util namespace

--- beans/src/main/scala/org/springframework/scala/beans/factory/xml/UtilNamespaceHandler.scala
+++ beans/src/main/scala/org/springframework/scala/beans/factory/xml/UtilNamespaceHandler.scala
@@ -4,7 +4,7 @@ import org.springframework.util.StringUtils
 import org.w3c.dom.Element
 import org.springframework.beans.factory.xml.{ParserContext, AbstractSingleBeanDefinitionParser, NamespaceHandlerSupport}
 import org.springframework.beans.factory.support.BeanDefinitionBuilder
-import org.springframework.scala.beans.factory.config.SeqFactoryBean
+import org.springframework.scala.beans.factory.config.{SetFactoryBean, SeqFactoryBean}
 
 /**
  * @author Arjen Poutsma
@@ -14,21 +14,49 @@ class UtilNamespaceHandler extends NamespaceHandlerSupport {
 	private final val SCOPE_ATTRIBUTE: String = "scope"
 
 	def init() {
-		registerBeanDefinitionParser("seq", new SeqBeanDefinitionParser());
+		registerBeanDefinitionParser("seq", new SeqBeanDefinitionParser())
+		registerBeanDefinitionParser("set", new SetBeanDefinitionParser())
 	}
 
 	private class SeqBeanDefinitionParser extends AbstractSingleBeanDefinitionParser {
 
 		protected override def getBeanClass(element: Element) = classOf[SeqFactoryBean[_]]
 
-		protected override def doParse(element: Element, parserContext: ParserContext, builder: BeanDefinitionBuilder) {
-			val parsedList: java.util.List[_] = parserContext.getDelegate.parseListElement(element, builder.getRawBeanDefinition)
+		protected override def doParse(element: Element,
+		                               parserContext: ParserContext,
+		                               builder: BeanDefinitionBuilder) {
+			val parsedList: java.util.List[_] = parserContext.getDelegate
+					.parseListElement(element, builder.getRawBeanDefinition)
 			builder.addConstructorArgValue(parsedList)
-/*
-			if (StringUtils.hasText(listClass)) {
-				builder.addPropertyValue("targetListClass", listClass)
+			/*
+//			val listClass: String = element.getAttribute("list-class")
+				 if (StringUtils.hasText(listClass)) {
+					 builder.addPropertyValue("targetListClass", listClass)
+				 }
+	 */
+			val scope: String = element.getAttribute(SCOPE_ATTRIBUTE)
+			if (StringUtils.hasLength(scope)) {
+				builder.setScope(scope)
 			}
-*/
+		}
+	}
+
+	private class SetBeanDefinitionParser extends AbstractSingleBeanDefinitionParser {
+
+		protected override def getBeanClass(element: Element): Class[_] = classOf[SetFactoryBean[_]]
+
+		protected override def doParse(element: Element,
+		                               parserContext: ParserContext,
+		                               builder: BeanDefinitionBuilder) {
+			val parsedSet: java.util.Set[_] = parserContext.getDelegate
+					.parseSetElement(element, builder.getRawBeanDefinition)
+			builder.addConstructorArgValue(parsedSet)
+			/*
+				 val setClass: String = element.getAttribute("set-class")
+				 if (StringUtils.hasText(setClass)) {
+					 builder.addPropertyValue("targetSetClass", setClass)
+				 }
+	 */
 			val scope: String = element.getAttribute(SCOPE_ATTRIBUTE)
 			if (StringUtils.hasLength(scope)) {
 				builder.setScope(scope)
--- beans/src/test/scala/org/springframework/scala/beans/factory/xml/UtilNamespaceHandlerTest.scala
+++ beans/src/test/scala/org/springframework/scala/beans/factory/xml/UtilNamespaceHandlerTest.scala
@@ -11,11 +11,19 @@ class UtilNamespaceHandlerTest extends FunSuite {
 	val applicationContext = new ClassPathXmlApplicationContext("utilNamespaceHandlerTest.xml", getClass)
 
 	test("seq") {
-		val seq = applicationContext.getBean("seq", classOf[scala.collection.Seq[_]])
+		val seq = applicationContext.getBean("seq", classOf[scala.collection.Seq[String]])
 		assert(seq != null)
 		assert(seq.size == 2)
 		assert(seq.contains("one"))
 		assert(seq.contains("two"))
+	}
+
+	test("set") {
+		val set = applicationContext.getBean("set", classOf[scala.collection.Set[String]])
+		assert(set != null)
+		assert(set.size == 2)
+		assert(set.contains("one"))
+		assert(set.contains("two"))
 	}
 
 }
