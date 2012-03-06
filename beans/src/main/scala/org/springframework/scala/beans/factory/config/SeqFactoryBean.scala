5d81db17622c035ec1f0134784f757ec50623386
Arjen Poutsma <apoutsma@vmware.com>
2012-03-06 15:33:56 +0100

    Working on scala-util namespace

--- beans/src/main/scala/org/springframework/scala/beans/factory/config/SeqFactoryBean.scala
+++ beans/src/main/scala/org/springframework/scala/beans/factory/config/SeqFactoryBean.scala
@@ -2,21 +2,28 @@ package org.springframework.scala.beans.factory.config
 
 import org.springframework.beans.factory.config.AbstractFactoryBean
 import scala.collection.mutable.Builder
+import scala.collection.Seq
 
 /**
+ * Simple factory for shared [[scala.collection.Seq]] instances. Allows for central setup
+ * of sequences via the "`seq`" element in XML bean definitions.
+ *
  * @author Arjen Poutsma
+ * @tparam T  the element type of the collection
+ * @param sourceSeq the source sequence, typically populated via XML "seq" elements
+ * @param builderFunction function used to create a new sequence builder
  */
-class SeqFactoryBean[T](val sourceSeq: scala.collection.Seq[T], 
+class SeqFactoryBean[T](val sourceSeq: Seq[T],
                         val builderFunction: () => Builder[T, Seq[T]])
 		extends AbstractFactoryBean[scala.collection.Seq[T]] {
 
-	def this(sourceSeq: scala.collection.Seq[T]) {
-		this(sourceSeq, scala.collection.Seq.newBuilder[T] _)
+	def this(sourceSeq: Seq[T]) {
+		this(sourceSeq, Seq.newBuilder[T] _)
 	}
 
-	override def getObjectType = classOf[scala.collection.Seq[T]]
+	override def getObjectType = classOf[Seq[T]]
 
-	override def createInstance(): scala.collection.Seq[T] = {
+	override def createInstance(): Seq[T] = {
 		val builder = builderFunction()
 		// TODO: determine Seq element type by using GenericCollectionTypeResolver
 		builder ++= sourceSeq
--- beans/src/main/scala/org/springframework/scala/beans/factory/config/SetFactoryBean.scala
+++ beans/src/main/scala/org/springframework/scala/beans/factory/config/SetFactoryBean.scala
@@ -4,18 +4,28 @@ import org.springframework.beans.factory.config.AbstractFactoryBean
 import collection.mutable.Builder
 
 /**
+ * Simple factory for shared [[scala.collection.Set]] instances. Allows for central setup
+ * of sequences via the "`set`" element in XML bean definitions.
+ *
  * @author Arjen Poutsma
+ * @tparam T  the element type of the collection
+ * @param sourceSet the source set, typically populated via XML "set" elements
+ * @param builderFunction function used to create a new set builder
  */
-class SetFactoryBean[T](val sourceSeq: scala.collection.Set[T],
-                        val builderFunction: () => Builder[T, Set[T]] = scala.collection.Set.newBuilder[T] _)
+class SetFactoryBean[T](val sourceSet: scala.collection.Set[T],
+                        val builderFunction: () => Builder[T, Set[T]])
 		extends AbstractFactoryBean[scala.collection.Set[T]] {
+
+	def this(sourceSet: scala.collection.Set[T]) {
+		this(sourceSet, scala.collection.Set.newBuilder[T] _)
+	}
 
 	override def getObjectType = classOf[scala.collection.Set[T]]
 
 	override def createInstance(): scala.collection.Set[T] = {
 		val builder = builderFunction()
 		// TODO: determine Set element type by using GenericCollectionTypeResolver
-		builder ++= sourceSeq
+		builder ++= sourceSet
 		builder.result()
 	}
 }
