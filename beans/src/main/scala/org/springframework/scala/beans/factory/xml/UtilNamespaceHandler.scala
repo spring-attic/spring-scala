e84592b599438548854c8343f575b0f1bc3dcdd7
Arjen Poutsma <apoutsma@vmware.com>
2012-03-06 10:30:19 +0100

    Working on namespace

---
+++ beans/src/main/scala/org/springframework/scala/beans/factory/xml/UtilNamespaceHandler.scala
@@ -0,0 +1,8 @@
+package org.springframework.scala.beans.factory.xml
+
+/**
+ * @author Arjen Poutsma
+ */
+class UtilNamespaceHandler {
+
+}
---
+++ beans/src/test/scala/org/springframework/scala/beans/factory/xml/UtilNamespaceHandlerTest.scala
@@ -0,0 +1,10 @@
+package org.springframework.scala.beans.factory.xml
+
+import org.scalatest.FunSuite
+
+/**
+ * @author Arjen Poutsma
+ */
+class UtilNamespaceHandlerTest extendsFunSuite {
+
+}
