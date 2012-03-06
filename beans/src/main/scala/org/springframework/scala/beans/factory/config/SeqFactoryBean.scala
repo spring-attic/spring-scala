da7757fec5ea5e3841b40c91519d264234f0c92c
Arjen Poutsma <apoutsma@vmware.com>
2012-03-06 10:34:00 +0100

    Working on namespace

--- beans/src/main/scala/org/springframework/scala/beans/factory/config/SeqFactoryBean.scala
+++ beans/src/main/scala/org/springframework/scala/beans/factory/config/SeqFactoryBean.scala
@@ -1,8 +1,30 @@
 package org.springframework.scala.beans.factory.config
 
+import org.springframework.beans.factory.config.AbstractFactoryBean
+import scala.collection.mutable.Builder
+import scala.collection.JavaConversions._
+
 /**
  * @author Arjen Poutsma
  */
-class SeqFactoryBean {
+class SeqFactoryBean[T](val sourceSeq: scala.collection.Seq[T], 
+                        val builderFunction: () => Builder[T, Seq[T]])
+		extends AbstractFactoryBean[scala.collection.Seq[T]] {
 
+	def this(sourceSeq: scala.collection.Seq[T]) {
+		this(sourceSeq, scala.collection.Seq.newBuilder[T] _)
+	}
+
+	def this(sourceSeq: java.util.List[T]) {
+		this(asScalaBuffer(sourceSeq), scala.collection.Seq.newBuilder[T] _)
+	}
+
+	override def getObjectType = classOf[scala.collection.Seq[T]]
+
+	override def createInstance(): scala.collection.Seq[T] = {
+		val builder = builderFunction()
+		// TODO: determine Seq element type by using GenericCollectionTypeResolver
+		builder ++= sourceSeq
+		builder.result()
+	}
 }
--- beans/src/test/scala/org/springframework/scala/beans/factory/xml/UtilNamespaceHandlerTest.scala
+++ beans/src/test/scala/org/springframework/scala/beans/factory/xml/UtilNamespaceHandlerTest.scala
@@ -1,10 +1,31 @@
 package org.springframework.scala.beans.factory.xml
 
 import org.scalatest.FunSuite
+import org.springframework.context.support.ClassPathXmlApplicationContext
 
 /**
  * @author Arjen Poutsma
  */
-class UtilNamespaceHandlerTest extendsFunSuite {
+class UtilNamespaceHandlerTest extends FunSuite {
+
+	val applicationContext = new ClassPathXmlApplicationContext("utilNamespaceHandlerTest.xml", getClass)
+
+	test("seq") {
+		val seq = applicationContext.getBean("seq", classOf[scala.collection.Seq[_]])
+		assert(seq != null)
+		assert(seq.size == 2)
+		assert(seq.contains("one"))
+		assert(seq.contains("two"))
+	}
+
+	test("list") {
+		val list = applicationContext.getBean("list", classOf[java.util.List[_]])
+		assert(list != null)
+		assert(list.size == 2)
+		assert(list.contains("one"))
+		assert(list.contains("two"))
+	}
+
+
 
 }
