0c63ac1b279422b0aa01ca4eedfb3b5efc6363fd
hekonsek <hekonsek@gmail.com>
2013-01-09 14:21:00 +0100

    Added AOP Advice Wrappers
    Added a collection of implicit conversions that convert advice functions
    into *Advice classes usable by Spring AOP.  Based on hekonsek initial
    pull requests, with the following changes:
    
    - Renamed MethodAdvice to AdviceConversions
    - Removed explicit methods in favor of implicit versions
    - Added Scaladoc

---
+++ src/test/scala/org/springframework/scala/aop/AdviceConversionsTests.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2011-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.scala.aop
+
+import org.springframework.context.support.GenericApplicationContext
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.springframework.scala.context.function.FunctionalConfigApplicationContext
+
+@RunWith(classOf[JUnitRunner])
+class AdviceConversionsTests extends FunSuite {
+
+  val applicationContext: GenericApplicationContext = new FunctionalConfigApplicationContext(classOf[MethodAdviceConfiguration])
+
+  test("MethodAdvice#interceptor") {
+    val intercepted = applicationContext.getBean("intercepted")
+    val toStringResult = intercepted.toString
+    assert("intercepted" eq toStringResult)
+  }
+
+  test("MethodAdvice#before") {
+    val beforeAdviced = applicationContext.getBean("advicedBefore")
+    intercept[BeforeAdviceException](beforeAdviced.toString)
+  }
+
+  test("MethodAdvice#after") {
+    val advicedAfter = applicationContext.getBean("advicedAfter")
+    intercept[AfterAdviceException](advicedAfter.toString)
+  }
+
+
+}
