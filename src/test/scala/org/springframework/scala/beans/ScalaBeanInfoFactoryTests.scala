09e392fd0292705ab7faff63c7a1c21cb6fc8b5a
Arjen Poutsma <apoutsma@vmware.com>
2013-01-22 12:46:12 +0100

    Upgraded to Scala 2.10
    - Updated build script
    - Replaced Manifest.erasure with Manifest.runtimeClass and/or TypeTag
    - Removed ScalaObject check from ScalaBeanInfoFactory, since ScalaObject
      has been removed in 2.10
    - Replaced Manifest usage with TypeTag

--- src/test/scala/org/springframework/scala/beans/ScalaBeanInfoFactoryTests.scala
+++ src/test/scala/org/springframework/scala/beans/ScalaBeanInfoFactoryTests.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2012 the original author or authors.
+ * Copyright 2011-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,7 +44,7 @@ class ScalaBeanInfoFactoryTests extends FunSuite {
 
 	test("supports") {
 		val factory = new ScalaBeanInfoFactory
-		assert(null != factory.getBeanInfo(classOf[ScalaObject]))
+		assert(null != factory.getBeanInfo(classOf[ScalaBean]))
 		assert(null == factory.getBeanInfo(classOf[Object]))
  }
 
