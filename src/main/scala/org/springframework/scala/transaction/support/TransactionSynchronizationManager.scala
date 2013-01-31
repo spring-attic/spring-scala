ccf1604c72a66c3006540fe128a7380ce13ed036
hekonsek <hekonsek@gmail.com>
2013-01-31 12:47:39 +0100

    Added Scaladoc to the TransactionSynchronizationManager.

--- src/main/scala/org/springframework/scala/transaction/support/TransactionSynchronizationManager.scala
+++ src/main/scala/org/springframework/scala/transaction/support/TransactionSynchronizationManager.scala
@@ -3,8 +3,23 @@ package org.springframework.scala.transaction.support
 import org.springframework.transaction.support.TransactionSynchronization
 import org.springframework.transaction.support.{TransactionSynchronizationManager => DelegateSynchronizationManager}
 
+/**
+ * Scala-based convenience wrapper for the Spring
+ * [[org.springframework.transaction.support.TransactionSynchronizationManager]], providing pattern matching style
+ * callbacks.
+ *
+ * @author Henryk Konsek
+ * @since 1.0
+ */
 object TransactionSynchronizationManager {
 
+  /**
+   * Register partial function delegate for the [[org.springframework.transaction.support.TransactionSynchronization]]
+   * callbacks.
+   *
+   * @param synchronization partial function representing callback to be executed when particular
+   * [[org.springframework.scala.transaction.support.SynchronizationEvent]] is fired.
+   */
   def registerSynchronization(synchronization: PartialFunction[SynchronizationEvent, Unit]) {
     def propagateEvent(e: SynchronizationEvent) {
       if (synchronization.isDefinedAt(e))
