cbb8783599e6fb3d05accd7abb0ccc85e7b7f82c
Henryk Konsek <hekonsek@gmail.com>
2013-04-11 21:01:05 -0400

    Added componentScan to ContextSupport
    This commit adds support for component scanning to the ContextSupport
    class. Effectively, this functionality is similar to the
    'component-scan' feature of the context Spring Namespace.

---
+++ src/test/scala/org/springframework/scala/context/function/ComponentScanTests.scala
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2011-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.scala.context.function
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{GivenWhenThen, FunSuite}
+import org.scalatest.matchers.ShouldMatchers
+import org.springframework.stereotype.Component
+import org.springframework.context.annotation.{AnnotationScopeMetadataResolver, ScopedProxyMode}
+import org.springframework.core.`type`.filter.RegexPatternTypeFilter
+import java.util.regex.Pattern
+import org.springframework.beans.factory.NoSuchBeanDefinitionException
+
+@RunWith(classOf[JUnitRunner])
+class ComponentScanTests extends FunSuite with ShouldMatchers with GivenWhenThen {
+
+  // Fixtures
+
+  var context : FunctionalConfigApplicationContext = _
+
+  // Tests
+
+  test("should load components with var arg componentScan") {
+    Given("loaded config")
+    context = FunctionalConfigApplicationContext[VarArgComponentScanConfig]
+
+    When("scanned component is retrieved")
+    val scannedComponent = context.getBean(classOf[TestComponent])
+
+    Then("component should not be null")
+    scannedComponent should not be (null)
+  }
+
+  test("should load components with basic componentScan") {
+    Given("loaded config")
+    context = FunctionalConfigApplicationContext[BasicComponentScanConfig]
+
+    When("scanned component is retrieved")
+    val scannedComponent = context.getBean(classOf[TestComponent])
+
+    Then("component should not be null")
+    scannedComponent should not be (null)
+  }
+
+  test("should validate conflicting scope configuration") {
+    When("loaded config")
+    evaluating {
+      FunctionalConfigApplicationContext[ScopeConflictComponentScanConfig]
+    } should produce[IllegalArgumentException]
+  }
+
+  test("should exclude from scan") {
+    Given("loaded config")
+    context = FunctionalConfigApplicationContext[ExcludingComponentScanConfig]
+
+    When("scanned component is retrieved")
+    evaluating {
+      context.getBean(classOf[TestComponent])
+    } should produce[NoSuchBeanDefinitionException]
+  }
+
+  test("should include in scan") {
+    Given("loaded config")
+    context = FunctionalConfigApplicationContext[IncludingComponentScanConfig]
+
+    When("scanned component is retrieved")
+    val scannedComponent = context.getBean(classOf[TestComponent])
+
+    Then("component should not be null")
+    scannedComponent should not be (null)
+  }
+
+}
+
+// Configuration classes
+
+class BasicComponentScanConfig extends FunctionalConfiguration with ContextSupport {
+
+  componentScan(basePackages = Seq("org.springframework.scala.context.function"))
+
+}
+
+class VarArgComponentScanConfig extends FunctionalConfiguration with ContextSupport {
+
+  componentScan("org.springframework.scala.context.function")
+
+}
+
+class ScopeConflictComponentScanConfig extends FunctionalConfiguration with ContextSupport {
+
+  componentScan(basePackages = Seq("org.springframework.scala.context.function"),
+    scopedProxy = Some(ScopedProxyMode.TARGET_CLASS), scopeResolver = Some(new AnnotationScopeMetadataResolver))
+
+}
+
+class ExcludingComponentScanConfig extends FunctionalConfiguration with ContextSupport {
+
+  componentScan(basePackages = Seq("org.springframework.scala.context.function"),
+    excludeFilters = Seq(new RegexPatternTypeFilter(Pattern.compile(".*Component"))))
+
+}
+
+class IncludingComponentScanConfig extends FunctionalConfiguration with ContextSupport {
+
+  componentScan(basePackages = Seq("org.springframework.scala.context.function"),
+    includeFilters = Seq(new RegexPatternTypeFilter(Pattern.compile(".*Component"))))
+
+}
+
+// Test component
+
+@Component
+class TestComponent {
+}
