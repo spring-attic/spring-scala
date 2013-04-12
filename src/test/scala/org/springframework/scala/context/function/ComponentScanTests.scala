d64b6730675d5f0adcdfc329b62aed4786f6f1cb
Henryk Konsek <hekonsek@gmail.com>
2013-04-11 21:01:05 -0400

    Added component scan mini DSL.

---
+++ src/test/scala/org/springframework/scala/context/function/ComponentScanTests.scala
@@ -0,0 +1,12 @@
+package org.springframework.scala.context.function
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hekonsek
+ * Date: 4/10/13
+ * Time: 9:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class ComponentScanTests {
+
+}
