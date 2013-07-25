3b0879bd0ec87b27b1b370b3c6f63ef948f70218
Maciej Zientarski <maciej.zientarski@gmail.com>
2013-07-25 23:26:09 +0200

    Imports cleanup

--- src/test/scala/org/springframework/scala/transaction/function/TransactionSupportTests.scala
+++ src/test/scala/org/springframework/scala/transaction/function/TransactionSupportTests.scala
@@ -3,15 +3,13 @@ package org.springframework.scala.transaction.function
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{BeforeAndAfterEach, FunSuite}
-import org.springframework.context.support.GenericApplicationContext
-import org.springframework.beans.factory.support.DefaultBeanNameGenerator
 import org.springframework.transaction.support.{DefaultTransactionStatus, AbstractPlatformTransactionManager, TransactionSynchronizationManager}
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.transaction.TransactionDefinition
 import org.springframework.aop.config.AopConfigUtils
 import org.springframework.aop.framework.autoproxy.InfrastructureAdvisorAutoProxyCreator
 import org.springframework.transaction.config.TransactionManagementConfigUtils
-import org.springframework.scala.context.function.{MyFunctionalConfiguration, FunctionalConfigApplicationContext, FunctionalConfiguration}
+import org.springframework.scala.context.function.{FunctionalConfigApplicationContext, FunctionalConfiguration}
 
 /**
  * @author Maciej Zientarski
